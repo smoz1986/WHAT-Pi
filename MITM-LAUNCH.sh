0652cd98fd08ff174344bc8b3b478196f303b50b
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-29 00:07:53 +0000

    Update MITM-LAUNCH.sh

--- MITM-LAUNCH.sh
+++ MITM-LAUNCH.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gnome-terminal -e ./MITM-ETTERCAP.sh --working-directory=/root/WHAT-Pi && gnome-terminal -e ./SSLSTRIP.sh —working-directory=/root/WHAT-Pi
+nome-terminal -e ./MITM-ETTERCAP.sh --working-directory=/root/WHAT-Pi && gnome-terminal -e ./SSLSTRIP.sh —working-directory=/root/WHAT-Pi && gnome-terminal -e ./dns2proxy.sh --working-directory=/root/WHAT-Pi
