372b739f4c8ab10ab1e16ecbdac3881c9363858a
smoz1986 <smoz1986@users.noreply.github.com>
2016-02-29 01:00:30 +0000

    Update WHAT-installer.sh

--- WHAT-installer.sh
+++ WHAT-installer.sh
@@ -5,9 +5,10 @@ echo installing WHAT-Pi
 apt-get update && apt-get upgrade -y
 apt-get install python-pip python-setuptools git git-core gpsd gpsd-clients python-m2crypto libgcrypt11-dev libnl-3-dev libnl-genl-3-dev openvpn kali-linux—wireless ettercap-text-only -y
 rm -r /etc/ettercap/etter.conf
-cp /root/WHAT-Pi/etter.conf /etc/ettercap/etter.conf
+cp /root/WHAT-Pi/dns2proxy.sh /root/dns2proxy.sh 
 git clone https://github.com/byt3bl33d3r/sslstrip2.git
+cd /root/
 git clone https://github.com/LeonardoNve/dns2proxy.git
-cd sslstrip2 && python setup.py install && cd ..
+cd /root/WHAT-Pi/sslstrip2 && python setup.py install
 echo All finished
 
